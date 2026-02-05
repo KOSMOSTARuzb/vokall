6b56b1188ae6fa0211e90683ea4de2be7e1462f0
KOSMOSTARuzb <94840242+KOSMOSTARuzb@users.noreply.github.com>
2026-02-06 00:29:14 +0500

    Cleanup

--- app/src/main/java/uz/kosmostar/vokall/Constants.java
+++ app/src/main/java/uz/kosmostar/vokall/Constants.java
@@ -2,6 +2,5 @@ package uz.kosmostar.vokall;
 
 /** A set of constants used within the app. */
 public class Constants {
-  /** A tag for logging. Use 'adb logcat -s WalkieTalkie' to follow the logs. */
-  public static final String TAG = "WalkieTalkie";
+  public static final String TAG = "Vokall";
 }
